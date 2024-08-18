7113acd70a55949235adbf7413eafadcb8a774a6
haodeon <31973188+haodeon@users.noreply.github.com>
2024-08-18 14:11:44 +1200

    initial commit

---
+++ Network/Program.fs
@@ -0,0 +1,15 @@
+module Cloud.Network
+
+open Pulumi.FSharp
+
+let infra () =
+  //
+  // Add your resources here.
+  //
+
+  // Export outputs here.
+  dict []
+
+[<EntryPoint>]
+let main _ =
+  Deployment.run infra
---
+++ Ops/Ops.fs
@@ -0,0 +1,21 @@
+﻿[<AutoOpen>]
+module Cloud.Ops
+
+open Pulumi
+
+let getConfigOrDefault (config: Config) =
+    fun key defaultValue ->
+        match config.Get key with
+        | null -> defaultValue
+        | value -> value
+
+let getOutputDetails (stackRef: StackReference) =
+    fun convert outputKey ->
+        task {
+            // Use GetOutputDetailsAsync and return the result
+            let! stackOutput = stackRef.GetOutputDetailsAsync(outputKey)
+            return convert stackOutput.Value
+        }
+
+let objToString (obj: obj) = obj :?> string
+let objToFloat (obj: obj) = obj :?> float
